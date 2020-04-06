dc4dffc9745bf3458cbcf625c7de5b5116a891dd
trinhmaingoc <trinhmn.dlhp@gmail.com>
2020-04-06 23:08:30 +0700

    deploy

---
+++ build/precache-manifest.7d0a513d6bc512987ec9b84585c82168.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5642a362e296d8acd9202fefcbcdf2f3",
+    "url": "/lesson04-props/index.html"
+  },
+  {
+    "revision": "5ee62a2ce52900e1deac",
+    "url": "/lesson04-props/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6bfa518c7bd3d42224b7",
+    "url": "/lesson04-props/static/js/2.4f3804e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lesson04-props/static/js/2.4f3804e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ee62a2ce52900e1deac",
+    "url": "/lesson04-props/static/js/main.f3ad1173.chunk.js"
+  },
+  {
+    "revision": "5699867e64a02676b86f",
+    "url": "/lesson04-props/static/js/runtime-main.50646b12.js"
+  }
+]);
